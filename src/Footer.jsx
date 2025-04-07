5fd17f633ac4558b9788482c7de88eadefd0b50f
Suganya Prabagarane <suganyaprabagarane@gmai.com>
2025-04-07 14:00:51 +0300

    add step1 task

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () =>{
+    return(
+        <footer>
+        <p>Copyright@REACT25K</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () =>{
+    return(
+        <header>
+        <h1>HR APP</h1>
+        </header>
+    )
+}
+
+export default Header;
